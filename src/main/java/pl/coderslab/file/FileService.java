1471e3f7aef57f676d52ec71693a6f45e1ceba43
apreisne <apreisne@visa.com>
2025-06-29 13:33:59 +0200

    added red color to true or false option

--- src/main/java/pl/coderslab/file/FileService.java
+++ src/main/java/pl/coderslab/file/FileService.java
@@ -1,5 +1,7 @@
 package pl.coderslab.file;
 
+import pl.coderslab.utils.ConsoleColors;
+
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -29,7 +31,8 @@ public class FileService {
         int counter = 1;
 
         for (String[] strings : tasks) {
-            System.out.printf("%d : %s%n", counter, String.join(", ", strings));
+            System.out.printf(ConsoleColors.RESET + "%d : %s", counter, String.join(", ", strings[0], strings[1]));
+            System.out.printf(", %s%n", ConsoleColors.RED + strings[2]);
             counter++;
         }
     }
